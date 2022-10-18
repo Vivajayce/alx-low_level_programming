310e7a4405d1b14eb0d110f926d7c7c2e00df7d3
Vivajayce <joyacha4real@gmail.com>
2022-10-18 14:56:55 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - checks if the charcter is lower case
+ * @c: the character
+ * Return: 1 if the letter is lower case, 0 if not
+ */
+int _islower(int c)
+{
+int ten;
+char jo;
+for (ten = 0; ten <= 9; ten++)
+{
+for (jo = '9'; jo <= 'z'; jo++)
+_putchar(jo);
+_putchar('\n');
+}
+}
