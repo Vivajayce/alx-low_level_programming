33fe173a485777503fd92ef6c68a9b523fbd906a
Vivajayce <joyacha4real@gmail.com>
2022-10-20 14:47:51 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
- * print_numbers - print the numbers from 0 to 9
+ * 3-print_numbers - print the numbers from 0 to 9
  * Description: You can only use _putchar twice
- * Return: 0 always
  */
 void print_numbers(void)
 {
-int i = 0;
+int i;
+i = 0;
 while (i < 10)
 {
 _putchar(i + '0');
+
 i++;
+
 }
+
 _putchar('\n');
-return (0);
+
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,4 +13,5 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 int _putchar(char c);
+
 #endif
