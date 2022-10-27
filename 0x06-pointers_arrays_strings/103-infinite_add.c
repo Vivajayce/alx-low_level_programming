111395db1cc8e73a77f2b54bf72d89cd602f2d48
Vivajayce <joyacha4real@gmail.com>
2022-10-27 10:04:34 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -33,25 +33,3 @@ else if (tens && r_index < 0)
 return (0);
 return (r + r_index + 1);
 }
-/**
- * infinite_add - Adds two numbers
- * @n1: The first number to be added
- * @n2: The second number to be added
- * @r: the buffer to store the results
- * @size_r: the size of the buffer
- * Return: if rcan store the sum of numbers - a pointer to the result
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-int index, n1_len = 0, n2_len = 0;
-for (index = 0; *(n1 + index); index++)
-n1_len++;
-for (index = 0; *(n2 + index); index++)
-n2_len++;
-if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-return (0);
-n1 += n1_len - 1;
-n2 += n2_len - 1;
-*(r + size_r) = '\0';
-return (add_strings(n1, n2, r, --size_r));
-}
