5a5ddca62538150a0d342c1b2892ae7aedcf9e2c
Vivajayce <joyacha4real@gmail.com>
2022-10-27 15:25:49 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,18 +7,19 @@
  */
 void print_buffer(char *b, int size)
 {
-int byte, index;
+int byte;
+int index;
 for (byte = 0; byte < size; byte += 10)
 {
-printf("%08x: ", byte);
+printf("%08x:  ", byte);
 for (index = 0; index < 10; index++)
 {
 if ((index + byte) >= size)
-printf("  ");
+printf(" ");
 else
 printf("%02x", *(b + index + byte));
 if ((index % 2) != 0 && index != 0)
-printf("  ");
+printf(" ");
 }
 for (index = 0; index < 10; index++)
 {
@@ -27,7 +28,7 @@ break;
 else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
 printf("%c", *(b + index + byte));
 else
-printf(" . ");
+printf(".");
 }
 if (byte >= size)
 continue;
