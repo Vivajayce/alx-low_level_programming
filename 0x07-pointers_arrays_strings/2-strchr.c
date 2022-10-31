be88aac72e7b2395951eb85772eeb9a87be0a387
Vivajayce <joyacha4real@gmail.com>
2022-10-31 05:30:27 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locate character in a string
+ * @s: character array string
+ * @c: charcter to look for
+ * Return: NULL if charcter not found, or pointer to first occurence of charcter c
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+else if (*(s + 1) == c)
+return (s + 1);
+s++;
+}
+return (s + 1);
+}
