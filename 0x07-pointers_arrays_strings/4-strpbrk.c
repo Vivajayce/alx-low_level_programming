2689cf535b915988c345e0c457c64b6b8ea2a5d6
Vivajayce <joyacha4real@gmail.com>
2022-10-31 05:58:30 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strpbrk - search a string for any set of bytes
+ * @s: string
+ * @accept: string to match
+ * Return: pointer to the byte in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+int j;
+char *p;
+i = 0;
+while (s[i] != '\0')
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (accept[j] == s[i])
+{
+p = &s[i];
+return (p);
+}
+j++;
+}
+i++;
+}
+return (0);
+}
