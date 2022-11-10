e9e149e6c9941651f0636ba9369592c86ba49b71
Vivajayce <joyacha4real@gmail.com>
2022-11-10 12:50:19 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ size2++;
 }
 if (n > size2)
 n = size2;
-p = malloc((size1 + n + 1) + sizeof(char));
+p = malloc((size1 + n + 1) * sizeof(char));
 if (p == NULL)
 return (0);
 for (i = 0; i < size1; i++)
