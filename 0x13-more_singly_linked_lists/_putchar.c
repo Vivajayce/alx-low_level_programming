4db722a046a05fde548b16f86f73fee4dced0282
Vivajayce <joyacha4real@gmail.com>
2022-11-29 12:29:06 -0800

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the charcter c to stdout
+ * @c: the charcter to print
+ * Return: on success 1
+ */
+int _putchar (char c)
+{
+return (write(1, &c, 1));
+}
