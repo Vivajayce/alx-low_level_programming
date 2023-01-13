ef6289599d20d01decb5db8f850b5b6b2a644442
Vivajayce <joyacha4real@gmail.com>
2023-01-13 13:03:57 -0800

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,51 +1,41 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_idx - insert a new node at given position
- * @h: double pointer to head
- * @idx: index to insert into
- * @n: value to store in new node
- * Return: Address of new node, or NULL if failed
+ * insert_dnodeint_at_index - function with three arguments
+ * @h: pointer to doubly linked list
+ * @idx: index position to insert node
+ * @n: value of new node
+ * Description: inserting new node into a doubly linked list
+ * Return: address of new node or NULL if failed
  */
-dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-unsigned int c;
-dlistint_t *tmp, *prev, *new;
+dlistint_t *new, *cursor;
+if (h == NULL)
+return (NULL);
+if (idx == 0)
+return (add_dnodeint(h, n))
+/* loop until nth node of idx */
+cursor = *h;
+while (idx > 1 && cursor && cursor->next)
+{
+cursor = cursor->next;
+idx--;
+}
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 return (NULL);
+if (idx > 1 || cursor == NULL)
+return (NULL);
+
+/* assign value to new node */
 new->n = n;
-for (tmp = *h, c = 1; tmp && c < idx; c++, tmp = tmp->next)
-prev = tmp;
-if (idx == 0)
-{
-*h = new; new->prev = NULL;
-new->next = (tmp == NULL) ? NULL : tmp;
-return (new);
-}
-if (idx == 1)
-{
-prev = *h;
-tmp = ((*h)->next == NULL) ? NULL : (*h)->next;
-new->prev = prev; new->next = tmp; prev->next = new;
-if (tmp)
-tmp->prev = new;
-return (new);
-}
-if (idx == c && tmp == NULL)
-{
-if (prev != NULL)
-{
-new->prev = prev; new->next = NULL;
-prev->next = new; return (new);
-}
-free(new); return (NULL);
-}
-else if (idx != c && tmp == NULL)
-{
-free(new); return (NULL);
-}
-prev = tmp; tmp = tmp->next; new->prev = prev;
-new->next = tmp; prev->next = new; tmp->prev = new;
+
+/* insert node */
+if (cursor->next != NULL)
+cursor->next->prev = new;
+new->prev = cursor;
+new->next = cursor->next;
+cursor->next = new;
 return (new);
 }
